28f3de1f1f26ee99163845d4abbf9cbccac351c7
Javier Nieto <58864261+JavierNLSauces@users.noreply.github.com>
2020-11-05 22:40:58 +0100

    Add proyectoDWES

---
+++ indexProyectoDWES.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+ <!-- 
+    Autor.- Javier Nieto 
+    Fecha de modificacion.- 2020/09/29
+  -->
+
+<html lang="es">
+<head>
+    <title>Proyecto DWES - Javier Nieto Lorenzo</title> 
+    <meta charset="UTF-8">
+    <meta name="viewport"   content="width=device-width, initial-scale=1.0">
+    <meta name="author"     content="Javier Nieto Lorenzo">
+    <meta name="robots"     content="index, follow">      
+    <link rel="stylesheet"  href="webroot/css/estilos.css"       type="text/css" >
+    <link rel="icon"        href="webroot/media/favicon.ico"    type="image/x-icon">
+</head>
+
+<body>
+    <header>
+        <h1>DESARROLLO WEB EN ENTORNO SERVIDOR </h1>
+        <a class="github" href="https://github.com/JavierNLSauces/" target="_blank"><img  src="webroot/media/github.png" alt="github icon"></a>
+    </header>
+
+    <nav>
+        <ul>
+            <li>
+                <a href="../index.html">HOME</a> 
+            </li>
+            <li>
+                <a href="#">DAW</a>
+            </li>
+            <li>
+                <a href="#">DIW</a>
+            </li>
+            <li>
+                <a href="#">DWEC</a>
+            </li>
+            <li class="active">
+                <a href="indexProyectoDWES.php">DWES</a>  
+            </li>
+        </ul>
+    </nav>
+
+    <main>
+        <ul class="content">
+            <li><a href="doc/EstudioTema1.pdf" target="_blank">TEMA 1: DESARROLLO WEB EN ENTORNO SERVIDOR</a></li>
+            <li><a href="doc/EstudioTema2.pdf" target="_blank">TEMA 2: INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ENTORNO DE DESARROLLO Y DEL ENTORNO DE EXPLOTACIÓN</a></li>
+            <li><a href="../proyectoTema3/indexProyectoTema3.php">TEMA 3: CARACTERÍSTICAS DEL LENGUAJE PHP</a></li>
+            <li><a href="../proyectoTema4/indexProyectoTema4.php">TEMA 4: TÉCNICAS DE ACCESO A DATOS EN PHP</a></li>
+        </ul>
+    </main>
+
+    <footer>
+        
+        <address> &copy; 2020-2021 Javier Nieto Lorenzo </address>
+    </footer>
+</body>
+</html>
